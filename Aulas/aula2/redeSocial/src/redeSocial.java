a0796958029dbe817201e1c6e81be72c92441f82
EzekLOBO <eloliveira.cic@uesc.br>
2023-11-14 20:42:08 -0300

    Aula rede social

--- Aulas/aula2/redeSocial/src/redeSocial.java
+++ Aulas/aula2/redeSocial/src/redeSocial.java
@@ -1,53 +1,72 @@
-
 import java.util.Scanner;
 
 public class redeSocial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        User user = new User();
+        Cliente cliente = new Cliente();
+
         System.out.print("Digite o nome do usuário: ");
-        user.setName(scanner.nextLine());
+        cliente.setNome(scanner.nextLine());
+
         System.out.print("Digite a idade do usuário: ");
-        user.setAge(scanner.nextInt());
+        cliente.setIdade(scanner.nextInt());
         scanner.nextLine();
+
         System.out.print("Digite o endereço do usuário: ");
-        user.setAddress(scanner.nextLine());
+        cliente.setEndereco(scanner.nextLine());
+
         System.out.print("Digite a profissão do usuário: ");
-        user.setProfession(scanner.nextLine());
+        cliente.setProfissao(scanner.nextLine());
+
         System.out.println("Usuário criado com sucesso!");
-        System.out.println("Nome: " + user.getName());
-        System.out.println("Idade: " + user.getAge());
-        System.out.println("Endereço: " + user.getAddress());
-        System.out.println("Profissão: " + user.getProfession());
+        System.out.println("Nome: " + cliente.getNome());
+        System.out.println("Idade: " + cliente.getIdade());
+        System.out.println("Endereço: " + cliente.getEndereco());
+        System.out.println("Profissão: " + cliente.getProfissao());
+
         System.out.println("Digite algumas postagens:");
         String postagem = scanner.nextLine();
-        user.adicionaPostagem(postagem);
+        cliente.adicionaPostagem(postagem);
+
+        System.out.println("Digite a pontuação inicial do usuário: ");
+        int pontuacaoInicial = scanner.nextInt();
+        cliente.setPontuacao(pontuacaoInicial);
+
+        System.out.println("Pontuação inicial: " + cliente.getPontuacao());
+
+        System.out.println("Digite a pontuação a ser adicionada: ");
+        int pontuacaoDelta = scanner.nextInt();
+        cliente.alteraPontuacao(pontuacaoDelta);
+
+        System.out.println("Pontuação atualizada: " + cliente.getPontuacao());
+
         scanner.close();
     }
 }
 
-class User {
-    private String name;
-    private int age;
-    private String address;
-    private String profession;
+class Cliente {
+    private String nome;
+    private int idade;
+    private String endereco;
+    private String profissao;
     private String[] postagens = new String[10];
     private int numPostagens = 0;
+    private int pontuacao = 0;
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
     }
 
-    public void setProfession(String profession) {
-        this.profession = profession;
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
     }
 
     public void adicionaPostagem(String postagem) {
@@ -59,20 +78,20 @@ class User {
         }
     }
 
-    public String getName() {
-        return name;
+    public String getNome() {
+        return nome;
     }
 
-    public int getAge() {
-        return age;
+    public int getIdade() {
+        return idade;
     }
 
-    public String getAddress() {
-        return address;
+    public String getEndereco() {
+        return endereco;
     }
 
-    public String getProfession() {
-        return profession;
+    public String getProfissao() {
+        return profissao;
     }
 
     public String[] getPostagens() {
@@ -81,5 +100,21 @@ class User {
 
     public int getNumPostagens() {
         return numPostagens;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public void alteraPontuacao(int delta) {
+        if (pontuacao + delta >= 0) {
+            pontuacao += delta;
+        } else {
+            System.out.println("A pontuação não pode ficar menor que zero.");
+        }
     }
 }
