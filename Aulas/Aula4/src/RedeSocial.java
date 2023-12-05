75a2d80bc947884b12ba37f373103076f8a3d5a6
EzekLobo <eloliveira.cic@uesc.br>
2023-12-05 15:54:36 -0300

    .

--- Aulas/Aula4/src/RedeSocial.java
+++ Aulas/Aula4/src/RedeSocial.java
@@ -1,4 +1,138 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
-public class RedeSocial {
+class RedeSocial {
 
+    class Usuario {
+        private static int proximoId = 1; // Variável estática para gerar IDs únicos
+        private int id;
+        private String nome;
+        private String email;
+        private String senha;
+        private List<Usuario> amigos; // Adicionando a lista de amigos
+
+        public Usuario(String nome, String email, String senha) {
+            this.id = proximoId++;
+            this.nome = nome;
+            this.email = email;
+            this.senha = senha;
+            this.amigos = new ArrayList<>();
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getSenha() {
+            return senha;
+        }
+
+        public List<Usuario> getAmigos() {
+            return amigos;
+        }
+
+        public void criarAmizade(Usuario amigo) {
+            amigos.add(amigo);
+            amigo.amigos.add(this);
+        }
+
+        public void destruirAmizade(Usuario amigo) {
+            amigos.remove(amigo);
+            amigo.amigos.remove(this);
+        }
+    }
+
+    class ListaUsuarios {
+        private List<Usuario> usuarios;
+
+        public ListaUsuarios() {
+            this.usuarios = new ArrayList<>();
+        }
+
+        public void criarUsuario() { // Modified method to prompt for user information
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite o nome do novo usuário: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Digite o email do novo usuário: ");
+            String email = scanner.nextLine();
+
+            System.out.print("Digite a senha do novo usuário: ");
+            String senha = scanner.nextLine();
+
+            Usuario novoUsuario = new Usuario(nome, email, senha);
+            usuarios.add(novoUsuario);
+            System.out.println("Novo usuário criado com sucesso!");
+        }
+
+        public void removerUsuario(Usuario usuario) {
+            usuarios.remove(usuario);
+        }
+
+        public boolean contemUsuario(Usuario usuario) {
+            return usuarios.contains(usuario);
+        }
+
+        public List<Usuario> getUsuarios() {
+            return usuarios;
+        }
+    }
+
+    class Amizade {
+        private ListaUsuarios listaUsuarios;
+
+        public Amizade(ListaUsuarios listaUsuarios) {
+            this.listaUsuarios = listaUsuarios;
+        }
+
+        public void criarAmizade(Usuario usuario1, Usuario usuario2) {
+            if (!listaUsuarios.contemUsuario(usuario1) || !listaUsuarios.contemUsuario(usuario2)) {
+                System.out.println("Usuários não encontrados na lista.");
+                return;
+            }
+
+            usuario1.criarAmizade(usuario2);
+            System.out.println("Amizade criada entre " + usuario1.getNome() + " (ID: " + usuario1.getId() + ") e " + usuario2.getNome() + " (ID: " + usuario2.getId() + ")");
+        }
+
+        public void destruirAmizade(Usuario usuario1, Usuario usuario2) {
+            if (!listaUsuarios.contemUsuario(usuario1) || !listaUsuarios.contemUsuario(usuario2)) {
+                System.out.println("Usuários não encontrados na lista.");
+                return;
+            }
+
+            usuario1.destruirAmizade(usuario2);
+            System.out.println("Amizade destruída entre " + usuario1.getNome() + " (ID: " + usuario1.getId() + ") e " + usuario2.getNome() + " (ID: " + usuario2.getId() + ")");
+        }
+    }
+    public static void main(String[] args) {
+
+        ListaUsuarios listaUsuarios = new ListaUsuarios();
+
+        listaUsuarios.criarUsuario(); // Calling the modified method to create a user
+        listaUsuarios.criarUsuario();
+
+        Amizade amizade = new Amizade(listaUsuarios);
+
+        // Example of how to use the friendship relationship
+        Usuario usuario1 = listaUsuarios.getUsuarios().get(0);
+        Usuario usuario2 = listaUsuarios.getUsuarios().get(1);
+
+        amizade.criarAmizade(usuario1, usuario2);
+
+        // Exemplo de como usar a relação de amizade
+        System.out.println("Usuários na amizade: " + usuario1.getNome() + " (ID: " + usuario1.getId() + ") e " + usuario2.getNome() + " (ID: " + usuario2.getId() + ")");
+
+        amizade.destruirAmizade(usuario1, usuario2);
+    }
 }
---
+++ POs/Prova/Hospital.java
@@ -0,0 +1,252 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Medicamento {
+    private String nome;
+
+    public Medicamento(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
+
+class Insumo {
+    private String nome;
+
+    public Insumo(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
+
+class Procedimento {
+    private String nome;
+    private List<Medicamento> medicamentos;
+    private List<Insumo> insumos;
+
+    public Procedimento(String nome) {
+        this.nome = nome;
+        this.medicamentos = new ArrayList<>();
+        this.insumos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void adicionarMedicamentos(List<Medicamento> medicamentos) {
+        this.medicamentos.addAll(medicamentos);
+    }
+
+    public void adicionarInsumos(List<Insumo> insumos) {
+        this.insumos.addAll(insumos);
+    }
+}
+
+class AgenteSaude {
+    private String nome;
+
+    public AgenteSaude(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
+
+class Medico extends AgenteSaude {
+    private String especialidade;
+
+    public Medico(String nome, String especialidade) {
+        super(nome);
+        this.especialidade = especialidade;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+}
+
+class Enfermeiro extends AgenteSaude {
+    public Enfermeiro(String nome) {
+        super(nome);
+    }
+}
+
+class Paciente {
+    private String nome;
+
+    public Paciente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
+
+public class Triagem {
+    private Paciente paciente;
+    private AgenteSaude enfermeiro;
+    private String tipoAtendimento;
+
+    public Triagem(Paciente paciente, AgenteSaude enfermeiro, String tipoAtendimento) {
+        this.paciente = paciente;
+        this.enfermeiro = enfermeiro;
+        this.tipoAtendimento = tipoAtendimento;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public AgenteSaude getEnfermeiro() {
+        return enfermeiro;
+    }
+
+    public String getTipoAtendimento() {
+        return tipoAtendimento;
+    }
+
+    // Método para identificar o paciente na triagem
+    public void identificarPaciente() {
+      
+    }
+
+    // Método para cadastrar o paciente após a identificação
+    public void cadastrarPaciente() {
+     
+    }
+
+    // Método para identificar o tipo de atendimento na triagem
+    public void identificarTipoAtendimento() {
+        
+    }
+}
+
+
+class Atendimento {
+    private Paciente paciente;
+    private AgenteSaude medico;
+
+    public Atendimento(Paciente paciente, AgenteSaude medico) {
+        this.paciente = paciente;
+        this.medico = medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public AgenteSaude getMedico() {
+        return medico;
+    }
+}
+
+class RegistroProcedimento {
+    private Procedimento procedimento;
+    private AgenteSaude medico;
+    private AgenteSaude enfermeiro;
+
+    public RegistroProcedimento(Procedimento procedimento, AgenteSaude medico, AgenteSaude enfermeiro) {
+        this.procedimento = procedimento;
+        this.medico = medico;
+        this.enfermeiro = enfermeiro;
+    }
+
+    public Procedimento getProcedimento() {
+        return procedimento;
+    }
+
+    public AgenteSaude getMedico() {
+        return medico;
+    }
+
+    public AgenteSaude getEnfermeiro() {
+        return enfermeiro;
+    }
+}
+
+class ProcedimentoParte {
+    private Procedimento procedimentoPai;
+    private Procedimento procedimentoFilho;
+
+    public ProcedimentoParte(Procedimento procedimentoPai, Procedimento procedimentoFilho) {
+        this.procedimentoPai = procedimentoPai;
+        this.procedimentoFilho = procedimentoFilho;
+    }
+
+    public Procedimento getProcedimentoPai() {
+        return procedimentoPai;
+    }
+
+    public Procedimento getProcedimentoFilho() {
+        return procedimentoFilho;
+    }
+}
+
+public class HospitalSystem {
+    private List<Medicamento> medicamentos = new ArrayList<>();
+    private List<Insumo> insumos = new ArrayList<>();
+    private List<Procedimento> procedimentos = new ArrayList<>();
+    private List<AgenteSaude> agentesSaude = new ArrayList<>();
+    private List<Paciente> pacientes = new ArrayList<>();
+    private List<Triagem> triagens = new ArrayList<>();
+    private List<Atendimento> atendimentos = new ArrayList<>();
+    private List<RegistroProcedimento> registrosProcedimentos = new ArrayList<>();
+    private List<ProcedimentoParte> procedimentosPartes = new ArrayList<>();
+
+    public void cadastrarMedicamento(Medicamento medicamento) {
+        medicamentos.add(medicamento);
+    }
+
+    public void cadastrarInsumo(Insumo insumo) {
+        insumos.add(insumo);
+    }
+
+    public void cadastrarProcedimento(Procedimento procedimento) {
+        procedimentos.add(procedimento);
+    }
+
+    public void cadastrarAgenteSaude(AgenteSaude agenteSaude) {
+        agentesSaude.add(agenteSaude);
+    }
+
+    public void cadastrarPaciente(Paciente paciente) {
+        pacientes.add(paciente);
+    }
+
+    public void registrarTriagem(Triagem triagem) {
+        triagens.add(triagem);
+    }
+
+    public void registrarAtendimento(Atendimento atendimento) {
+        atendimentos.add(atendimento);
+    }
+
+    public void registrarProcedimento(Procedimento procedimento, AgenteSaude medico, AgenteSaude enfermeiro,
+                                      List<Medicamento> medicamentosUsados, List<Insumo> insumosUsados) {
+        RegistroProcedimento registro = new RegistroProcedimento(procedimento, medico, enfermeiro);
+        registrosProcedimentos.add(registro);
+
+        procedimento.adicionarMedicamentos(medicamentosUsados);
+        procedimento.adicionarInsumos(insumosUsados);
+    }
+
+    public void registrarProcedimentoParte(Procedimento procedimentoPai, Procedimento procedimentoFilho) {
+        ProcedimentoParte procedimentoParte = new ProcedimentoParte(procedimentoPai, procedimentoFilho);
+        procedimentosPartes.add(procedimentoParte);
+    }
+
+    public static void main(String[] args) {
+    
+       
+    }
+}
